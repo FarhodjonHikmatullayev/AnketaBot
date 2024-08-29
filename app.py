339f3ad6ac5039bff58ed4c5b5c2acf34faafc58
Farhodjon <farhodjonhikmatullayev@gmail.com>
2024-08-29 16:13:45 +0500

    First commit

--- app.py
+++ app.py
@@ -1,17 +1,28 @@
 from aiogram import executor
-
-from loader import dp
+import logging
+from loader import dp, db, bot
 import middlewares, filters, handlers
 from utils.notify_admins import on_startup_notify
 from utils.set_bot_commands import set_default_commands
+from keep_alive import keep_alive
+from data.config import DEVELOPMENT_MODE, BOT_TOKEN
+
+if not DEVELOPMENT_MODE:
+    keep_alive()
 
 
 async def on_startup(dispatcher):
+    # await bot.set_webhook(WEBHOOK_URL)
+    await db.create()
+    # await db.drop_users()
+
     # Birlamchi komandalar (/star va /help)
     await set_default_commands(dispatcher)
 
     # Bot ishga tushgani haqida adminga xabar berish
-    await on_startup_notify(dispatcher)
+    # await on_startup_notify(dispatcher)
+
+
 
 
 if __name__ == '__main__':
---
+++ utils/db_api/postgresql.py
@@ -0,0 +1,55 @@
+from typing import Union
+
+import asyncpg
+from asyncpg import Connection
+from asyncpg.pool import Pool
+
+from data import config
+from data.config import DEVELOPMENT_MODE
+
+
+class Database:
+
+    def __init__(self):
+        self.pool: Union[Pool, None] = None
+
+    async def create(self):
+        print('DEVELOPMENT_MODE', DEVELOPMENT_MODE)
+        if DEVELOPMENT_MODE:
+            self.pool = await asyncpg.create_pool(
+                user=config.DB_USER,
+                password=config.DB_PASS,
+                host=config.DB_HOST,
+                database=config.DB_NAME
+            )
+        else:
+            self.pool = await asyncpg.create_pool(
+                dsn=config.DATABASE_URL
+            )
+
+    async def execute(self, command, *args,
+                      fetch: bool = False,
+                      fetchval: bool = False,
+                      fetchrow: bool = False,
+                      execute: bool = False
+                      ):
+        async with self.pool.acquire() as connection:
+            connection: Connection
+            async with connection.transaction():
+                if fetch:
+                    result = await connection.fetch(command, *args)
+                elif fetchval:
+                    result = await connection.fetchval(command, *args)
+                elif fetchrow:
+                    result = await connection.fetchrow(command, *args)
+                elif execute:
+                    result = await connection.execute(command, *args)
+            return result
+
+    @staticmethod
+    def format_args(sql, parameters: dict):
+        sql += " AND ".join([
+            f"{item} = ${num}" for num, item in enumerate(parameters.keys(),
+                                                          start=1)
+        ])
+        return sql, tuple(parameters.values())
