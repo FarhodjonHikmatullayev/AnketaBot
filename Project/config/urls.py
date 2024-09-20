0650b4f1007aab10342caffe97cf77af89797f5b
Farhodjon <farhodjonhikmatullayev@gmail.com>
2024-09-20 16:08:24 +0500

    modified

--- Project/config/urls.py
+++ Project/config/urls.py
@@ -16,13 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('path-to-admin-site-secret8747/', admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('', include('Anketa.urls', namespace="Anketa")),
 ]
+
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
