012f173165a009bc01b56d4b3a2345bcf5a85e56
Farhodjon <farhodjonhikmatullayev@gmail.com>
2024-10-06 17:23:57 +0500

    modified

--- Project/Anketa/admin.py
+++ Project/Anketa/admin.py
@@ -1,13 +1,13 @@
-from django.contrib import admin
-from .models import *
-
-
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('id', 'first_name', 'last_name', 'username', 'phone', 'telegram_id')
-    search_fields = ('username', 'phone', 'telegram_id')
-
-
-@admin.register(Questionnaire)
-class QuestionnaireAdmin(admin.ModelAdmin):
-    list_display = ('id', 'first_name', 'last_name', 'created_at')
+# from django.contrib import admin
+# from .models import *
+#
+#
+# @admin.register(User)
+# class UserAdmin(admin.ModelAdmin):
+#     list_display = ('id', 'first_name', 'last_name', 'username', 'phone', 'telegram_id')
+#     search_fields = ('username', 'phone', 'telegram_id')
+#
+#
+# @admin.register(Questionnaire)
+# class QuestionnaireAdmin(admin.ModelAdmin):
+#     list_display = ('id', 'first_name', 'last_name', 'created_at')
