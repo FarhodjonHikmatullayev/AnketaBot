cfa56d7e230a14b73c4db063d2400b4d38d61a63
Farhodjon <farhodjonhikmatullayev@gmail.com>
2024-10-06 17:25:43 +0500

    modified

--- Project/Anketa/urls.py
+++ Project/Anketa/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from .views import questionnaire_view
+# from .views import questionnaire_view
 
 app_name = 'Anketa'
 
 urlpatterns = [
-    path('', questionnaire_view),
+    # path('', questionnaire_view),
 ]
