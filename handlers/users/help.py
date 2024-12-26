4d14ac179247a34054af1058af906bf9a6ca1d99
FarhodjonHikmatullayev <hikmatullayevfarhodjon@gmail.com>
2024-12-26 13:16:38 +0500

    Update help.py

--- handlers/users/help.py
+++ handlers/users/help.py
@@ -1,14 +1,14 @@
 from aiogram import types
 from aiogram.dispatcher.filters.builtin import CommandHelp
-
 from loader import dp
-
 
 @dp.message_handler(CommandHelp())
 async def bot_help(message: types.Message):
-    text = ("Buyruqlar: ",
-            "/start - Botni ishga tushirish",
-            "/help - Yordam",
-            "😊 Anketani telegram orqali to'ldirishda muammo bo'layotgan bo'lsa ushbu \nhttps://anketa.mega-center.uz/ sahifasiga \no'tib anketani to'ldiring")
+    text = (
+        "Buyruqlar: ",
+        "/start - Botni ishga tushirish",
+        "/help - Yordam",
+        "😊 Anketani telegram orqali to'ldirishda muammo bo'layotgan bo'lsa, ushbu \nhttps://anketa.mega-center.uz/ sahifasiga \no'tib anketani to'ldiring"
+    )
 
     await message.answer("\n".join(text))
