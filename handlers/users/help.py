6f3e6b380908298d6221db88db51b340873eed1a
FarhodjonHikmatullayev <hikmatullayevfarhodjon@gmail.com>
2024-12-26 12:43:53 +0500

    Update help.py

--- handlers/users/help.py
+++ handlers/users/help.py
@@ -8,6 +8,7 @@ from loader import dp
 async def bot_help(message: types.Message):
     text = ("Buyruqlar: ",
             "/start - Botni ishga tushirish",
-            "/help - Yordam")
-    
+            "/help - Yordam",
+            "�^=^s� Anketani telegram orqali to'ldirishda muammo bo'layotgan bo'lsa ushbu \nhttps://anketa.mega-center.uz/ sahifasiga \no'tib anketani to'ldiring")
+
     await message.answer("\n".join(text))
